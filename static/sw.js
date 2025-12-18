889a868ec6285360d7c4645ba851a9cb9f91d84c
KTK <kitsuka.asaki.38n@gmail.com>
2025-12-18 15:40:06 +0900

    Set notification badge to NapoleonIcon.png

--- static/sw.js
+++ static/sw.js
@@ -11,7 +11,8 @@ self.addEventListener('push', function (event) {
     const title = data.title || 'SSWordbook Notification';
     const options = {
         body: data.body || '新しい通知があります',
-        icon: '/static/pergamon/normal_logo.png?v=5',
+        icon: '/static/pergamon/normal_logo.png?v=6',
+        badge: '/static/NapoleonIcon.png?v=6',
         data: {
             url: data.url || '/'
         }
